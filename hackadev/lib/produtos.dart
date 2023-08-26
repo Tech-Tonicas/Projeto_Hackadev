d473a69e6a70b86a13e439431f55d08635c79189
KarineBF <113046943+KarineBF@users.noreply.github.com>
2023-08-26 13:20:28 -0300

    Atualização de preços, correção de nomes e caminhos dos produtos

--- hackadev/lib/produtos.dart
+++ hackadev/lib/produtos.dart
@@ -10,82 +10,82 @@ class ProdutosData {
   static final List<Produto> produtos = [
     Produto(
       nome: 'Fone Preto',
-      preco: 19.99,
+      preco: 249.99,
       urlImagem: 'imagens/fones/fonePreto.png',
     ),
     Produto(
       nome: 'Monitor Color',
-      preco: 29.99,
+      preco: 649.99,
       urlImagem: 'imagens/monitor/monitorColor.png',
     ),
     Produto(
       nome: 'Mouse Azul',
-      preco: 19.99,
+      preco: 149.99,
       urlImagem: 'imagens/mouse/mouseAzul.png',
     ),
     Produto(
       nome: 'Teclado RGB Azul',
-      preco: 19.99,
+      preco: 229.99,
       urlImagem: 'imagens/teclados/rgbAzul.png',
     ),
     Produto(
       nome: 'Fone Preto Azul',
-      preco: 19.99,
+      preco: 169.99,
       urlImagem: 'imagens/fones/fonePretoAzul.png',
     ),
     Produto(
       nome: 'Monitor Nature',
-      preco: 19.99,
+      preco: 489.99,
       urlImagem: 'imagens/monitor/monitorNature.png',
     ),
     Produto(
       nome: 'Mouse Colors',
-      preco: 19.99,
+      preco: 129.99,
       urlImagem: 'imagens/mouse/mouseColors.png',
     ),
     Produto(
       nome: 'Teclado RGB',
-      preco: 29.99,
+      preco: 389.29,
       urlImagem: 'imagens/teclados/tecladoRGB.png',
     ),
     Produto(
       nome: 'Fone Preto Marrom',
-      preco: 19.99,
+      preco: 189.99,
       urlImagem: 'imagens/fones/fonePretoMarrom.png',
     ),
     Produto(
       nome: 'Monitor Galaxia',
-      preco: 19.99,
+      preco: 229.99,
       urlImagem: 'imagens/monitor/monitorGalaxia.png',
     ),
     Produto(
-      nome: 'Monitor Preto',
-      preco: 19.99,
+      nome: 'Mouse Preto',
+      preco: 149.99,
       urlImagem: 'imagens/mouse/mousePreto.png',
     ),
     Produto(
       nome: 'Teclado Teclas Laranja',
-      preco: 19.99,
-      urlImagem: 'imagens/monitor/monitorStarWars.png',
+      preco: 49.99,
+      urlImagem: 'imagens/teclados/teclasLaranja.png',
     ),
     Produto(
       nome: 'Fone Rosa',
-      preco: 19.99,
+      preco: 189.99,
       urlImagem: 'imagens/fones/foneRosa.png',
     ),
     Produto(
       nome: 'Monitor Star Wars',
-      preco: 19.99,
+      preco: 599.99,
       urlImagem: 'imagens/monitor/monitorStarWars.png',
     ),
     Produto(
       nome: 'Mouse Roxo',
-      preco: 19.99,
+      preco: 49.99,
       urlImagem: 'imagens/mouse/mouseRoxo.png',
     ),
     Produto(
       nome: 'Teclado Teclas Vermelhas',
-      preco: 19.99,
+      preco: 69.99,
       urlImagem: 'imagens/teclados/teclasVermelhas.png',
     )
   ];
